1efe5b6b68a8655b638a689a0c28a2dce71553e5
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-11 20:10:28 +0530

    Tom and Jerry Chase

---
+++ JERRYCHASE.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+	
+	int t,x,y;
+	scanf("%d",&t);
+	
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    scanf("%d",&y);
+	    
+	    if(x<y)
+	    {
+	        printf("YES\n");
+	    }
+	    
+	    else 
+	    {
+	        printf("NO\n");
+	    }
+	}
+	
+	
+		return 0;
+}
+
