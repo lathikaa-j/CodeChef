c10ab9c2a69725256b3456b654c8d7ef2b0d2c4f
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-16 20:52:21 +0530

    Increase IQ

---
+++ INCRIQ.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int x,a;
+	
+	    scanf("%d",&x);
+	    a=x+7;
+	    if(a>170)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	
+	return 0;
+}
+
