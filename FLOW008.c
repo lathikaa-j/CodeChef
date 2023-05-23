e037eb1dc66408b59f2aee8277b9400912027f9d
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-23 16:49:18 +0530

    Helping Chef

---
+++ FLOW008.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+	int t,n;
+	scanf("%d",&t);
+	
+	while(t--)
+	{
+	   scanf("%d",&n); 
+	   
+	   if(n<10)
+	   {
+	       printf("Thanks for helping Chef!\n");
+	   }
+	   else
+	   {
+	       printf("-1\n");
+	   }
+	}
+	return 0;
+}
+
