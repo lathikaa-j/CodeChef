37c56388c551f833251476849b90f7430deaf7b7
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-24 16:11:52 +0530

    Is it a VOWEL or CONSONANT

---
+++ VOWELTB.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+	char c;
+	scanf("%c",&c);
+	switch(c)
+	{
+	    case 'A': printf("Vowel");
+	    break;
+	    
+	    case 'E': printf("Vowel");
+	    break;
+	    
+	    case 'I': printf("Vowel");
+	    break;
+	    
+	    case 'O': printf("Vowel");
+	    break;
+	    
+	    case 'U': printf("Vowel");
+	    break;
+	    default: printf("Consonant");
+	}
+	
+	return 0;
+}
+
