e9f4ce127f9341d977444469589bf5315cd44758
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-30 19:27:45 +0530

    Biryani classes

---
+++ BIRYANI.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int t,x,y,s;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&x);
+	    scanf("%d",&y);
+	    s=x*y;
+	    printf("%d\n",s);
+	}
+	return 0;
+}
+
