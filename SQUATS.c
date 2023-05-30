d7169fb1cf735eb278ca5a37d22e0d31cbe559b2
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-30 19:24:46 +0530

    Squats

---
+++ SQUATS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int t,x,s;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d",&x);
+        s=x*15;
+        printf("%d\n",s);
+    }
+	return 0;
+}
+
