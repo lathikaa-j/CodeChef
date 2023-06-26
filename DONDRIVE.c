8a84c9bff63be57af2daa63a028ed946c0baaa81
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-06-26 20:45:34 +0530

    Donation drive

---
+++ DONDRIVE.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int t,n,x,y;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d",&n);
+	    scanf("%d",&x);
+	    y=n-x;
+	    
+	  printf("%d\n",y);
+	   
+	}
+	return 0;
+}
+
